4c8837005d96c262808a12039f90da3db8feab88
Alexis Molina <braifer22@gmail.com>
2024-12-02 13:42:19 -0300

    Update GifItem test

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -4,16 +4,21 @@ import { GifItem } from '../../src/components/GifItem';
 
 describe('Pruebas <GifItem />', () => {
     const title = 'Alexis';
-    const src = 'https://one-punch.com/saitama.jpg';
+    const url = 'https://one-punch.com/saitama.jpg';
 
     test('Debe hacer match con el snapshot', () => {
-        const { container } = render(<GifItem title={title} src={src} />);
+        const { container } = render(<GifItem title={title} src={url} />);
         expect(container).toMatchSnapshot();
     });
 
     test('Debe mostrar la imagen con el SRC y el ALT indicado', () => {
-        render(<GifItem title={title} src={src} />);
-        expect(screen.getByRole('img').getAttribute('src')).toBe(src);
+        render(<GifItem title={title} src={url} />);
+        expect(screen.getByRole('img').getAttribute('src')).toBe(url);
         expect(screen.getByRole('img').getAttribute('alt')).toBe(title);
+    });
+
+    test('Debe mostrar el titulo en el componente', () => {
+        render(<GifItem title={title} src={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
     });
 });
