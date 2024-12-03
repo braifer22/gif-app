d23d32893a60d6fd55da352f6262e3a64af43a52
Alexis Molina <braifer22@gmail.com>
2024-12-03 07:47:19 -0300

    Add Header tests

--- tests/components/Header.test.jsx
+++ tests/components/Header.test.jsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vitest } from 'vitest';
 import { Header } from '../../src/components/Header';
 
 describe('Pruebas en <Header(>', () => {
@@ -10,5 +10,37 @@ describe('Pruebas en <Header(>', () => {
         fireEvent.input(input, { target: { value: 'Valorant' } });
 
         expect(input.value).toBe('Valorant');
+    });
+
+    test('Debe llamar onSubmit si el input tiene un valor', () => {
+        const inputValue = 'Alexis';
+        const onSubmit = vitest.fn();
+
+        render(<Header onSubmit={onSubmit} />);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, { target: { value: inputValue } });
+
+        fireEvent.submit(form);
+
+        expect(input.value).toBe('');
+        expect(onSubmit).toHaveBeenCalled();
+        expect(onSubmit).toHaveBeenCalledTimes(1);
+        expect(onSubmit).toHaveBeenCalledWith(inputValue);
+    });
+
+    test('No debe llamar onSubmit si el input está vacío', () => {
+        const onSubmit = vitest.fn();
+
+        render(<Header onSubmit={onSubmit} />);
+
+        const form = screen.getByRole('form');
+
+        fireEvent.submit(form);
+
+        expect(onSubmit).toHaveBeenCalledTimes(0);
+        expect(onSubmit).not.toHaveBeenCalled();
     });
 });
